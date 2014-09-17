2b6b88c36fbd5b28e225bc1f18335de15334b060
elena <elena.15@hotmail.it>
2014-09-17 08:52:21 +0100

    fixed code by adding the fill_station and created bike_container

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,41 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
 
-	def fill_station(station)
-		20.times { station.dock (Bike.new) }
-	end
-
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new(:capacity => 20) }
+	let(:station) { DockingStation.new(:capacity => 123) }
 	
-	it "should accept a bike" do
-	  expect(station.bike_count).to eq(0)
-	  station.dock(bike)
-	  expect(station.bike_count).to eq(1)
-    end
-
-    it "should release a bike" do
-      station.dock(bike)
-      station.release(bike)
-      expect(station.bike_count).to eq(0)
-    end
-
-    it "should know when it's full" do
-      expect(station).not_to be_full
-      fill_station station
-    end
-
-    it "should not accept a bike if it's full" do
-      fill_station station
-      expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
-    end
-
-    it "should provide the list of available bikes" do
-    	working_bike, broken_bike = Bike.new, Bike.new
-    	broken_bike.break!
-    	station.dock(working_bike)
-    	station.dock(broken_bike)
-    	expect(station.available_bikes).to eq ([working_bike])
-    end
+    it "should allow setting default capacity on initialising" do
+      expect(station.capacity).to eq(123)
+    end 
     	
 end
