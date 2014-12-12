91153f144b8d776161763c2decdc686af6869ce0
Elena <elena.15@hotmail.it>
2014-12-12 14:21:26 +0000

    refactored van

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,16 +5,11 @@ class DockingStation
 	include BikeContainer
 
 	def initialize(options = {})
-
 		self.capacity = options.fetch(:capacity, capacity)
-	
 	end
 
 	def accept_bikes_from(van)
-     	van.available_bikes.each do |bike|
-     		dock(bike)
-     		van.release bike
-        end
-     end
+   	van.available_bikes.each {|bike| dock(bike); van.release(bike)}
+  end
 
 end
