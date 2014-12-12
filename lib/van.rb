6f86e3b5502cc91b2c99f27602639f52d3052ad0
Elena <elena.15@hotmail.it>
2014-12-12 14:34:17 +0000

    refactored van

--- lib/van.rb
+++ lib/van.rb
@@ -4,19 +4,12 @@ class Van
 
 	 include BikeContainer
 
-     def collect_from(station)
-     	station.broken_bikes.each do |broken_bike|
-     		dock(broken_bike)
-     		station.release broken_bike
-        end
-     end
+   def collect_from(station)
+   	 station.broken_bikes.each { |broken_bike| dock(broken_bike); station.release broken_bike }
+   end
 
-     def pick_up_from(garage)
-     	garage.available_bikes.each do |fixed_bike|
-     		dock(fixed_bike)
-     		garage.release fixed_bike
-        end
-     end
+   def pick_up_from(garage)
+   	 garage.available_bikes.each { |fixed_bike| dock(fixed_bike); garage.release fixed_bike }
+   end
 
 end
-
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,23 +4,23 @@ require 'bike'
 
 describe Person do
 
-  let(:bike) {Bike.new}
-  let(:person) {Person.new}
+  let(:bike)    {Bike.new}
+  let(:person)  {Person.new}
   let(:station) {DockingStation.new}
 
-   it "should be able to rent a bike" do
-    station.dock(bike)
-    station.release(bike)
-    person.take(bike)
-    expect(person.bikes).to eq [bike]
-   end
-  
-   it "should be able to return the bike" do
-    person.take(bike)
-    person.release(bike)
-    station.dock(bike)
-    expect(person.bikes).to eq []
-    expect(station.bikes).to eq [bike]
-   end
+ it "should be able to rent a bike" do
+  station.dock(bike)
+  station.release(bike)
+  person.take(bike)
+  expect(person.bikes).to eq [bike]
+ end
+
+ it "should be able to return the bike" do
+  person.take(bike)
+  person.release(bike)
+  station.dock(bike)
+  expect(person.bikes).to eq []
+  expect(station.bikes).to eq [bike]
+ end
 
 end
