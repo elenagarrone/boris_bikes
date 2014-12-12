cfe670baa381e7c92c474ccc6d089b6a11747041
Elena <elena.15@hotmail.it>
2014-12-12 14:36:29 +0000

    fixed indentation in person spec

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,19 +8,19 @@ describe Person do
   let(:person)  {Person.new}
   let(:station) {DockingStation.new}
 
- it "should be able to rent a bike" do
-  station.dock(bike)
-  station.release(bike)
-  person.take(bike)
-  expect(person.bikes).to eq [bike]
- end
+  it "should be able to rent a bike" do
+    station.dock(bike)
+    station.release(bike)
+    person.take(bike)
+    expect(person.bikes).to eq [bike]
+  end
 
- it "should be able to return the bike" do
-  person.take(bike)
-  person.release(bike)
-  station.dock(bike)
-  expect(person.bikes).to eq []
-  expect(station.bikes).to eq [bike]
- end
+  it "should be able to return the bike" do
+    person.take(bike)
+    person.release(bike)
+    station.dock(bike)
+    expect(person.bikes).to eq []
+    expect(station.bikes).to eq [bike]
+  end
 
 end
