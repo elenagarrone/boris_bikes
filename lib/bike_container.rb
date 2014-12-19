4af2751df01df1b2662ca436c01ec31819956a4d
Elena Garrone <elena.15@hotmail.it>
2014-12-19 18:31:03 +0000

    fixed indentation in bike container

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -17,14 +17,14 @@ module BikeContainer
   end
 
   def dock(bike)
-  	raise "Station is full" if full?
+		raise "Station is full" if full?
     raise "Sorry, you didn't return a bike - try again" unless bike
-  	bikes << bike
+		bikes << bike
   end
 
   def release(bike)
     raise "Sorry, no bikes are available - try again later." if available_bikes == 0
-  	bikes.delete(bike)
+		bikes.delete(bike)
   end
 
   def release_available_bikes
@@ -36,7 +36,7 @@ module BikeContainer
   end
 
   def full?
-  	bike_count == capacity
+		bike_count == capacity
   end
 
   def empty?
@@ -44,7 +44,7 @@ module BikeContainer
   end
 
   def available_bikes
-  	bikes.reject &:broken?
+		bikes.reject &:broken?
   end
 
   def broken_bikes
