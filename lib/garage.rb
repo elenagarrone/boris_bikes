8f29621e28d4bc3757f6da03cb9cb5ff7ed8ebb3
Elena Garrone <elena.15@hotmail.it>
2014-12-19 18:40:24 +0000

    fixed indentation in garage

--- lib/garage.rb
+++ lib/garage.rb
@@ -2,15 +2,15 @@ require_relative 'bike_container'
 
 class Garage
 
- include BikeContainer
+  include BikeContainer
 
- def dock(bike)
- 	bike.fix!
- 	super
- end
+  def dock(bike)
+    bike.fix!
+    super
+  end
 
- def collect_from(van)
- 	van.broken_bikes.each { |broken_bike| dock(broken_bike); van.release broken_bike }
- end
+  def collect_from(van)
+    van.broken_bikes.each { |broken_bike| dock(broken_bike); van.release broken_bike }
+  end
 
 end
