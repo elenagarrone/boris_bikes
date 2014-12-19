754a40ab0f855ca00be39c298c539c4f0fd5e733
Elena Garrone <elena.15@hotmail.it>
2014-12-19 18:38:38 +0000

    fixed indentation in docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,14 +2,14 @@ require_relative 'bike_container'
 
 class DockingStation
 
-	include BikeContainer
+  include BikeContainer
 
-	def initialize(options = {})
-		self.capacity = options.fetch(:capacity, capacity)
-	end
+  def initialize(options = {})
+    self.capacity = options.fetch(:capacity, capacity)
+  end
 
-	def accept_bikes_from(van)
-   	van.available_bikes.each { |bike| dock(bike); van.release(bike) }
+  def accept_bikes_from(van)
+    van.available_bikes.each { |bike| dock(bike); van.release(bike) }
   end
 
 end
