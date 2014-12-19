6995d7c1b1cd2ddbacb22324bda485a60490ba3b
Elena Garrone <elena.15@hotmail.it>
2014-12-19 18:42:11 +0000

    fixed indentation van

--- lib/van.rb
+++ lib/van.rb
@@ -2,14 +2,14 @@ require_relative 'bike_container'
 
 class Van
 
-	 include BikeContainer
+  include BikeContainer
 
-   def collect_from(station)
-   	 station.broken_bikes.each { |broken_bike| dock(broken_bike); station.release broken_bike }
-   end
+  def collect_from(station)
+    station.broken_bikes.each { |broken_bike| dock(broken_bike); station.release broken_bike }
+  end
 
-   def pick_up_from(garage)
-   	 garage.available_bikes.each { |fixed_bike| dock(fixed_bike); garage.release fixed_bike }
-   end
+  def pick_up_from(garage)
+    garage.available_bikes.each { |fixed_bike| dock(fixed_bike); garage.release fixed_bike }
+  end
 
 end
