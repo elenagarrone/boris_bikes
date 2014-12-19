3d64d0580a881266b63860a7a7376d7b2db73ddf
Elena Garrone <elena.15@hotmail.it>
2014-12-19 18:46:57 +0000

    fixed indentation in garage spec

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -4,16 +4,16 @@ require 'bike'
 
 describe Garage do
 
-	let(:bike)        { Bike.new }
-	let(:garage)      { Garage.new }
-	let(:van)         { Van.new }
-	let(:broken_bike) { Bike.broken }
+  let(:bike)        { Bike.new }
+  let(:garage)      { Garage.new }
+  let(:van)         { Van.new }
+  let(:broken_bike) { Bike.broken }
 
   it "should collect only broken bikes from the van and fix them" do
-   van.dock(broken_bike)
-   garage.collect_from(van)
-   expect(garage.bikes).to eq [broken_bike]
-   expect(van.bikes).to eq []
+    van.dock(broken_bike)
+    garage.collect_from(van)
+    expect(garage.bikes).to eq [broken_bike]
+    expect(van.bikes).to eq []
   end
 
 end
